45821ce8389842c8e24e7c3dad914768129e453d
Lockmer <rickylockmer@gmail.com>
2021-06-10 13:33:18 -0600

    Add Activity and Project

---
+++ BugTracker/Models/Projects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class Projects
+    {
+        public int ID { get; set; }
+        public string ProjectName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+
+        public ICollection<Activity> Activities { get; set; }
+       
+
+    }
+}
